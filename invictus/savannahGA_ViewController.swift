a19df67bc9fac281cba10203eafc0f10cc091bc6
Ana Carvalho <anacarvalho1204@gmail.com>
2020-07-15 17:15:36 -0400

    added three view controllers for the different cities, along with mapkits for each one

---
+++ invictus/savannahGA_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  savannahGA_ViewController.swift
+//  invictus
+//
+//  Created by Ana Carvalho on 7/15/20.
+//  Copyright © 2020 Ana Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class savannahGA_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ invictus/washingtonDC_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  washingtonDC_ViewController.swift
+//  invictus
+//
+//  Created by Ana Carvalho on 7/15/20.
+//  Copyright © 2020 Ana Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class washingtonDC_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
